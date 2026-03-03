d12265c5cc862c7ea9081a585645221d38cde8fa
Eryn 📗 <erynclairesy@gmail.com>
2026-03-03 13:52:05 +0800

    Update 1

--- MachProj.c
+++ MachProj.c
@@ -19,6 +19,8 @@ Eryn Claire Go Sy, DLSU ID# 12506621
 #include <string.h>
 #include <time.h>
 
+#define MAX 50
+
 typedef char string[100];
 
 struct meal
@@ -28,14 +30,16 @@ struct meal
 	string unit;
 	float calories;
 };
+typedef struct meal mealType;
 
-void swap(candidateType *aCandidate, candidateType *bCandidate)
+struct recipe
 {
-	candidateType temp;
-	temp = *aCandidate;
-	*aCandidate = *bCandidate;
-	*bCandidate = temp;
-}
+	string name;
+	string class;
+	int servings
+	float calories;
+};
+typedef struct recipe recipeType;
 
 /*
 	Helper Function 1: Alphabetical Organizer
@@ -92,58 +96,95 @@ void Search()
 }
 
 /*
-	Function 1: Password Changer???
-	This function changes the user's username and password
-	Precondition: 
-	@param option is the user's input into the menu items
-	@returns 0, 1, 2, or 3 depending on what menu option matches the user's
+	Function 1: Username and Password Changer
+	This function changes the user's username and/or password
+	Precondition: option must be a single character
+	@param access is either the user's username or password
+	@param option is the user's input of what they want to change (e.g. 'U' for username, 'P' for password)
 */
-int MenuCheck(char option)
+void AccessModifier(char option)
 {
-	
+	string old_username, new_username, old_password, new_password;
+	do
+	{
+		if(option == 'U')
+		{
+			printf("Old username: ")
+			scanf("%s", old_password)
+			printf("New username: ")
+			scanf("%s", new_username)
+		}
+		else if(option == 'P')
+		{
+			printf("Old password: ")
+			scanf("%s", old_password)
+			printf("New password: ")
+			scanf("%s", new_password)
+		}
+		else
+			printf("Invalid input, please try again.\n")
+	}while(option != 'X');
 }
+
 
 /*
 	Function 2: Password Checker
 	This function checks for user's password
 	Precondition: option must be a single character
-	@param option is the user's input into the menu items
-	@returns 0 or 1 depending on whether or not the password matches
+	@param username - username input
+	@param sUsername - saved username
+	@param password - password input
+	@param sPassword - saved password
+	@returns 0 or 1 depending on whether or not both username and password match
 */
-int PassCheck(string password)
+int PassCheck(string username, string sUsername, string password, string sPassword)
 {
-	
+	int check = 0;
+	if((strcmp(username, sUsername) == 0) && (strcmp(password, sPassword) == 0))
+		check = 1;
+	return check;
 }
 
 /*
-	Function 3: Add Food Calorie Info
+	Function 3: Add food-calorie info
 	This function 
 	Precondition: 
-	@param option is the user's input into the menu items
+	@param aMeal - struct that holds a meal's name, quantity in certain units, and calorie count
 */
-void AddCalorie(string food, float quantity, string unit, float calories)
+void AddCalorie(mealType aMeal)
 {
-	
+	scanf("%s", aMeal[a]->food);
+	scanf("%f", &aMeal[a]->quantity);
+	scanf("%s", aMeal[a]->unit);
+	scanf("%f", &aMeal[a]->calories);
 }
 
 /*
-	Function 4: View Calorie Info
+	Function 4: View food-calorie chart
 	This function displays the food, its quantity, unit, and calorie count
 	Precondition: option must be a single character
-	@param food
-	@param quantity
-	@param unit
-	@param calories
+	@param aMeal - struct that holds a meal's name, quantity in certain units, and calorie count
 	@param option is either N for next display or X for exit
 */
-void ViewCalorie(string food, float quantity, string unit, float calories, char option)
+void ViewCalorie(mealType aMeal)
 {
+	int a = 0;
+	printf("		Food			Quantity	Unit	Calories");
+	while(a < 10)
+	{
+		printf("%s		%f		%s		%f\n", aMeal[a].food, aMeal[a].quantity, aMeal[a].unit, aMeal[a].calories);
+		a++;
+	}
+	printf("View next 10 items? Press 'N' to proceed; press 'X' to exit.\n")
 	while(option != 'X')
 	{
-		do
+		while(a % 10 != 0)
 		{
-			
-		}while();
+			printf("%s		%f		%s		%f\n", aMeal[a].food, aMeal[a].quantity, aMeal[a].unit, aMeal[a].calories);
+			a++;
+		}
+		printf("View next 10 items? Press 'N' to proceed; press 'X' to exit.\n")
+		scanf(" %c", option);
 	}
 }
 
@@ -155,22 +196,34 @@ void ViewCalorie(string food, float quantity, string unit, float calories, char 
 	@param quantity
 	@param unit
 	@param calories
+	@param aMeal - struct that holds a meal's name, quantity in certain units, and calorie count
 */
-void SaveCalorie(string food, float quantity, string unit, float calories)
+void SaveCalorie(mealType aMeal)
 {
-	
+	FILE *fp;
+	fp = fopen("Food-calorie information.txt", "a");
+	fprintf(fp, "%s\n", aMeal.food);
+	fprintf(fp, "%f ", aMeal->quantity);
+	fprintf(fp, "%s ", aMeal.unit);
+	fprintf(fp, "%f\n\n", aMeal->calories);
+	fclose(fp);
 }
 
 /*
 	Function 6: Load Calorie Info
 	This function checks for user's password
 	Precondition: option must be a single character
-	@param option is the user's input into the menu items
+	@param aMeal - struct that holds a meal's name, quantity in certain units, and calorie count
 */
-void LoadCalorie(string password)
+void LoadCalorie(mealType aMeal)
 {
-	
-}
+	FILE *fp;
+	fp = fopen("Food-calorie information.txt", "r");
+	fprintf(fp, "%s\n", aMeal.food);
+	fprintf(fp, "%f ", aMeal->quantity);
+	fprintf(fp, "%s ", aMeal.unit);
+	fprintf(fp, "%f\n\n", aMeal->calories);
+	fclose(fp);
 
 /*
 	Function 7: Add Recipe
@@ -331,29 +384,102 @@ void SearchByIngredient(string )
 
 int main()
 {
-	char option, key, garbage;
+	char main_option;
+	int box_option;
 	int i, n = 0;
-	candidateType candidate[20];
+	string username, password;
+	string sUsername, sPassword;
+	strcpy(sUsername, "admin");
+	strcpy(sPassword, "ad1234");
 	printf("******************Main Menu******************\n");
-	printf("U - Update recipe box\n");
-	printf("A - Access recipe box\n");
-	printf("E - Exit\n");
+	printf("[U] Update recipe box\n");
+	printf("[A] Access recipe box\n");
+	printf("[E] Exit\n");
 	printf("\nChoose a menu option from A-E: ");
 	scanf("%c", &option);
-	while(option != 'E')
+	while(main_option != 'E')
 	{
-		if(option == 'U')
+		if(main_option == 'U')
 		{
-			
+			printf("Username: ")
+			scanf("%s", username);
+			printf("Password: ")
+			scanf("%s", password);
+			while(box_option != 0)
+			{
+				printf("[1] Add food-calorie info\n");
+				printf("[2] View food-calorie chart\n");
+				printf("[3] Save calorie info\n");
+				printf("[4] Load calorie info\n");
+				printf("[5] Add recipe\n");
+				printf("[6] Modify recipe\n");
+				printf("[7] Delete recipe\n");
+				printf("[8] List recipe titles\n");
+				printf("[9] Display recipes\n");
+				printf("[10] Search recipe by title\n");
+				printf("[11] Export recipes\n");
+				printf("[12] Import recipes\n");
+				printf("[13] Change username and/or password\n");
+				printf("[0] Return to main menu\n");
+				
+				if(box_option == 1)
+					
+				else if(box_option == 2)
+					
+				else if(box_option == 3)
+					
+				else if(box_option == 4)
+					
+				else if(box_option == 5)
+					
+				else if(box_option == 6)
+					
+				else if(box_option == 7)
+					
+				else if(box_option == 8)
+					
+				else if(box_option == 9)
+					
+				else if(box_option == 10)
+					
+				else if(box_option == 11)
+					
+				else if(box_option == 12)
+					
+				else if(box_option == 13)
+					
+			}
 		}
-		else if(option == 'A')
+		else if(main_option == 'A')
 		{
-			
+			do
+			{
+				printf("[1] Import recipes\n");
+				printf("[2] List recipe titles\n");
+				printf("[3] Display recipes\n");
+				printf("[4] Scan recipes by ingredient\n");
+				printf("[5] Generate shopping list\n");
+				printf("[6] Recommend menu\n");
+				printf("[0] Return to main menu\n");
+				
+				if(box_option == 1)
+					
+				else if(box_option == 2)
+					
+				else if(box_option == 3)
+					
+				else if(box_option == 4)
+					
+				else if(box_option == 5)
+					
+				else if(box_option == 6)
+					
+			}while(box_option != 0);
 		}
-		else if(option != 'U' && option != 'A' && option != 'E')
+		else if(main_option != 'U' && main_option != 'A' && main_option != 'E')
 			printf("Invalid option.");
 		printf("\nChoose a menu option: ");
-		scanf(" %c", &option);
+		scanf(" %c", &main_option);
 	}
 	return 0;
 }
